ffec5bf7da8d84759b6f7ba2962832f7ab26bf94
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-07-14 10:06:18 +0530

    init

--- Whitelist(array).sol
+++ Whitelist(array).sol
@@ -7,6 +7,8 @@ contract Bank{
    mapping (address => uint64) public balances;
    
    address[] public accountAddress;
+   bool public Whitelisted;
+   address public index;
 
   
     event deposit( address accountAddress, uint64 balance);
@@ -14,12 +16,18 @@ contract Bank{
     event Transfer(address to,uint64 amount,uint64 balance );
     
     error InsufficientBalance(uint64 available, uint64 required);
+    error InsufficientBalanceinWithdraw(uint64 available, uint64 required);
 
 
-   
 
-    function Deposit(uint64 depositAmount, address index) public {
-     bool Whitelisted = false;
+   function whitelist()public{
+      require(Whitelisted = true, "not added");
+      accountAddress.push(index);
+   }
+
+    
+    function Deposit(uint64 depositAmount) public {
+     //bool Whitelisted = false;
      for (uint256 i = 0; i < accountAddress.length; i++) {
         if (accountAddress[i] == index) {
             Whitelisted = true;
@@ -32,12 +40,20 @@ contract Bank{
      emit deposit(msg.sender, depositAmount);
     }
 
+
     function withdraw(uint64 withdrawAmount) public {
      if (withdrawAmount <= balances[msg.sender]) {
-        balances[msg.sender] -= withdrawAmount;
+         balances[msg.sender] -= withdrawAmount;
+         revert InsufficientBalanceinWithdraw({
+            available: balances[msg.sender],
+            required: withdrawAmount
+        });
+        //balances[msg.sender] -= withdrawAmount;
+
         }
      emit witdraw(msg.sender,withdrawAmount);
     }
+
 
     function transfer(address to, uint64 amount) public {
      if (amount > balances[msg.sender])
