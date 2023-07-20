8289b0c8ed99425fc32bb0d8322ab3f48497837f
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-07-20 11:00:57 +0530

    init

--- ERC20.sol
+++ ERC20.sol
@@ -1,15 +1,82 @@
-
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.18;
 
+interface IERC20{
+   
+    function totalSupply() external view returns(uint256);
+    function balanceOf(address account)external view returns(uint256);
+       //return balances[msg.sender];
+    
 
-contract MyToken is ERC20{
-    constructor(string memory name, string memory symbol) ERC20(name,symbol) {
-        // 1.00 USD = 100
-        _mint(msg.sender,100*10**uint(decimals()));
-    }
+    function transfer(address to,uint256 amount)external returns (bool);
+
+    function allowance(address owner,address spender)external view returns (uint256);
+    function approve(address spender,uint256 amount) external returns(bool);
+    function transferFrom(address sender,address to,uint256 amount)external returns(bool);
+
+     event Transfer(address indexed from , address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender,uint256 value);
+
+    
 }
 
+contract MyToken is IERC20{
+    
+     mapping(address => uint64) public balance;
+     //mapping(address => mapping(uint => Todo)) public User;
+     mapping (address=>mapping(uint=>uint64)) public allowance;
 
+     uint256 public tokenSupply;
+    
+    // constructor(){
+        // string Name: smartSense;
+        // string Symbol: sS;
+        // uint256 Token_id: 219301280;
+        string smartSense;
+        string  sS;
+        //uint256 219301280;
+
+    // }
+
+    function totalSupply() external view returns(uint256){
+        //require(tokenSupply < amount);
+        return tokenSupply;
+    }
+
+    function transfer(address to,uint256 amount) external returns(bool) {
+        require(amount< balance[msg.sender]);
+        balance[msg.sender] -= amount;
+        balance[to] += amount;
+        emit Transfer(msg.sender,to, amount);
+        return true;
+        }
+    function  approve(address spender , uint256 amount) external returns(bool){
+        allowance[msg.sender][spender]= amount;
+        emit Approval(msg.sender,spender,amount);
+    }
+
+    function transferFrom(address sender, address to, uint256 amount) external returns(bool){
+        allowance[msg.sender][sender] -= amount;
+        balance[msg.sender[sender]] -= amount;
+        balance[msg.sender][to] += amount;
+        emit Transfer(sender, to, amount);
+    }
+
+    function balanceOf (address account)external view returns(uint256){
+        return balance[msg.sender];
+    }
+
+    function mint(address account, uint256 amount) public{
+      balance[msg.sender] += amount;
+      tokenSupply += amount;
+    }
+
+    function burn(address account, uint256 amount) public{
+       balance[msg.sender] -= amount;
+       tokenSupply -= amount; 
+    }
+
+    //function
+    
+}
